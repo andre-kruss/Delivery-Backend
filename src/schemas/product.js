ec276fc94cdc0b1840217a30a4a979ba3f8683be
henrique <luis.rodrigues@globant.com>
2021-02-23 15:21:54 -0300

    product schema description added

--- src/schemas/product.js
+++ src/schemas/product.js
@@ -2,6 +2,7 @@ import mongoose, { Schema } from 'mongoose'
 
 const ProductSchema = new mongoose.Schema({
   name: String,
+  description: String,
   price: Number,
   external_id: String,
   photo_url: String,
